430f8ae0be682b993c76b76f14b18cb0b5cf39d9
Introducción a la Programación 1 y 2 <mbrunettaalvarezper@mail.austral.edu.ar>
2025-09-19 11:29:29 -0300

    Update lists.py

--- lists.py
+++ lists.py
@@ -1,20 +1,18 @@
-# Replace the "ANSWER HERE" with your answer
-
 def remove_elements(list_to_remove_elements):
-    return "ANSWER HERE"  # Remove this line and implement
+    return 
 
 
 def add_elements(list_to_add_elements):
-    return "ANSWER HERE"  # Remove this line and implement
+    return 
 
 
 def is_empty(list_to_check):
-    return "ANSWER HERE"  # Remove this line and implement
+    return 
 
 
 def check_lists(list_to_compare1, list_to_compare2):
-    return "ANSWER HERE"  # Remove this line and implement
+    return 
 
 
 def list_of_lists(list_of_lists_to_modify):
-    return "ANSWER HERE"  # Remove this line and implement
+    return 
