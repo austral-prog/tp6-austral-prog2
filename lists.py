9a4d7ae8e0aa935a1855c2fcd019de5b7a80931e
Introducción a la Programación 1 y 2 <mbrunettaalvarezper@mail.austral.edu.ar>
2025-09-19 12:19:43 -0300

    Update lists.py

--- lists.py
+++ lists.py
@@ -22,7 +22,7 @@ def is_empty(list_to_check):
 
 
 def check_lists(list_to_compare1, list_to_compare2):
-    if (list_to_compare1 > 2) and (list_to_compare2 > 2):
+    if (len(list_to_compare1) > 2) and (len(list_to_compare2) > 2):
         if list_to_compare1[2] == list_to_compare2[2]:
             return True
     else:
