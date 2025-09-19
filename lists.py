7491a05ae21fa0a125ff3cf028371a10b0b0bace
Introducción a la Programación 1 y 2 <mbrunettaalvarezper@mail.austral.edu.ar>
2025-09-19 11:35:24 -0300

    Update lists.py

--- lists.py
+++ lists.py
@@ -2,15 +2,20 @@ def remove_elements(list_to_remove_elements):
     del list_to_remove_elements[5]
     del list_to_remove_elements[4]
     del list_to_remove_elements[0]
-    return 
+    return list_to_remove_elements
 
 
 def add_elements(list_to_add_elements):
-    return 
+    list_to_add_elements = insert(0, 'Pink')
+    list_to_add_elements = append('Yellow')
+    return list_to_add_elements
 
 
 def is_empty(list_to_check):
-    return 
+    if list_to_check == []:
+        return True
+    else:
+        return False
 
 
 def check_lists(list_to_compare1, list_to_compare2):
